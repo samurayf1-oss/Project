92afd070f2588da3dbf4448cbe3b19b37a3d84c1
samurayf1-oss <samuray.f1@mail.ru>
2026-05-05 04:24:08 +0700

    Use environment variables for API config

--- config.py
+++ config.py
@@ -3,8 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("PZUl1wN43Qgali3zYy")
-API_SECRET = os.getenv("JpO55xnttEfw7Ic20RJwrJeskMjst4SbxJ4u")
-
-if not API_KEY or not API_SECRET:
-    raise ValueError("BYBIT_API_KEY and BYBIT_API_SECRET must be set in .env")
+API_KEY = os.getenv("BYBIT_API_KEY", "")
+API_SECRET = os.getenv("BYBIT_API_SECRET", "")
